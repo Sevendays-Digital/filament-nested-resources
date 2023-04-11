f0d1a393175e908539322a4d55062176055e3682
haringsrob <haringsrob@users.noreply.github.com>
2023-04-11 13:48:20 +0000

    Fix styling

--- src/ResourcePages/NestedPageTrait.php
+++ src/ResourcePages/NestedPageTrait.php
@@ -4,8 +4,8 @@ namespace SevendaysDigital\FilamentNestedResources\ResourcePages;
 
 use Filament\Resources\Resource;
 use Illuminate\Database\Eloquent\Model;
-use SevendaysDigital\FilamentNestedResources\NestedResource;
 use Illuminate\Support\Str;
+use SevendaysDigital\FilamentNestedResources\NestedResource;
 
 trait NestedPageTrait
 {
